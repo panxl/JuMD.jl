09d69c6d90f50f143eebf93bc848a74a225afe6d
panxl <panxl@users.noreply.github.com>
2022-02-04 01:34:04 -0600

    Add reporters

--- src/reporters.jl
+++ src/reporters.jl
@@ -1,17 +1,54 @@
 abstract type AbstractReporter end
 
-struct StateReporter{T<:Integer} <: AbstractReporter
-    interval::T
+struct StateReporter <: AbstractReporter
+    interval::Int
+    path::String
 end
 
-struct CheckPointReporter{T<:Integer} <: AbstractReporter
-    interval::T
+function (reporter::StateReporter)(io::IO, system::AbstractSystem, current_step::Integer)
+    if current_step == 1
+        println(io, "# Step    T    Etot    Epot    Ekin")
+    end
+    if current_step % reporter.interval == 0
+        masses = mass(system)
+        velocities = velocity(system)
+        velocities_half = velocity_half(system)
+        ekin = 0.0
+        for (m, v) in zip(masses, velocities)
+            ekin += 0.5 * m * (v ⋅ v)
+        end
+        ekin_half = 0.0
+        for (m, v) in zip(masses, velocities_half)
+            ekin_half += 0.5 * m * (v ⋅ v)
+        end
+        T = 2 * ekin_half / (3 * length(masses) * KB)
+        epot = sum(system.force_groups.energies)
+        etot = epot + ekin
+        println(io, current_step, " ", T, " ",  etot, " ", epot, " ", ekin)
+        return nothing
+    end
 end
 
-struct NetCDFReporter{T<:Integer} <: AbstractReporter
-    interval::T
+struct CheckPointReporter <: AbstractReporter
+    interval::Int
 end
 
-struct CVReporter{T<:Integer} <: AbstractReporter
-    interval::T
+struct XYZTrajectoryReporter <: AbstractReporter
+    interval::Int
+    path::String
+end
+
+function (reporter::XYZTrajectoryReporter)(io::IO, system::AbstractSystem, current_step)
+    if current_step % reporter.interval == 0
+        positions = position(system)
+        atomic_numbers = atomic_number(system)
+        println(io, length(positions), "\n")
+        for (n, x) in zip(atomic_numbers, positions)
+            println(io, n, " ", x[1], " ", x[2], " ", x[3])
+        end
+    end
+end
+
+struct CVReporter <: AbstractReporter
+    interval::Int
 end
--- src/simulation.jl
+++ src/simulation.jl
@@ -1,9 +1,9 @@
 abstract type AbstractSimulation end
 
-struct Simulation{S<:AbstractSystem,I<:AbstractIntegrator,R<:AbstractReporter} <: AbstractSimulation
+struct Simulation{S<:AbstractSystem,I<:AbstractIntegrator,R} <: AbstractSimulation
     system::S
     integrator::I
-    reporters::Vector{R}
+    reporters::R
 end
 
 function Base.show(io::IO, ::MIME"text/plain", sim::Simulation)
@@ -13,7 +13,13 @@ function Base.show(io::IO, ::MIME"text/plain", sim::Simulation)
 end
 
 function Base.run(sim::AbstractSimulation, steps::Integer)
-    for _ in 1:steps
-        sim.integrator(sim.system)
+    files = map(reporter::AbstractReporter -> open(reporter.path, "w"), sim.reporters)
+    try
+        for i in 1:steps
+            sim.integrator(sim.system)
+            foreach((reporter::AbstractReporter, io::IO) -> reporter(io, sim.system, i), sim.reporters, files)
+        end
+    finally
+        close.(files)
     end
 end
