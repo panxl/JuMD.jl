40bf9bb2d60c8f8142d3a4f3eb546fe5634a0608
panxl <panxl@users.noreply.github.com>
2022-03-02 21:53:34 -0600

    Small update

--- src/forces/helpme/helpme.jl
+++ src/forces/helpme/helpme.jl
@@ -13,67 +13,102 @@ export XAligned, ShapeMatrix
 struct PMEInstance end
 
 function helpme_createD()
-    ccall((:helpme_createD, libhelpme), Ptr{PMEInstance}, ())
+    @ccall libhelpme.helpme_createD()::Ptr{PMEInstance}
 end
 
 function helpme_destroyD(pme::Ptr{PMEInstance})
-    ccall((:helpme_destroyD, libhelpme), Cvoid, (Ptr{PMEInstance},), pme,)
+    @ccall libhelpme.helpme_destroyD(pme::Ptr{PMEInstance})::Cvoid
 end
 
 function helpme_setupD(pme::Ptr{PMEInstance}, rPower, kappa, splineOrder, aDim, bDim, cDim, scaleFactor, nThreads)
-    ccall(
-        (:helpme_setupD, libhelpme), Cvoid,
-        (Ptr{PMEInstance}, Cshort, Cdouble, Cint, Cint, Cint, Cint, Cdouble, Cint),
-        pme, rPower, kappa, splineOrder, aDim, bDim, cDim, scaleFactor, nThreads,
-        )
+    @ccall libhelpme.helpme_setupD(
+        pme::Ptr{PMEInstance},
+        rPower::Cshort,
+        kappa::Cdouble,
+        splineOrder::Cint,
+        aDim::Cint,
+        bDim::Cint,
+        cDim::Cint,
+        scaleFactor::Cdouble,
+        nThreads::Cint,
+    )::Cvoid
 end
 
 function helpme_setup_compressedD(pme::Ptr{PMEInstance}, rPower, kappa, splineOrder, aDim, bDim, cDim, maxKA, maxKB, maxKC, scaleFactor, nThreads)
-    ccall(
-        (:helpme_setup_compressedD, libhelpme), Cvoid,
-        (Ptr{PMEInstance}, Cshort, Cdouble, Cint, Cint, Cint, Cint, Cint, Cint, Cint, Cdouble, Cint),
-        pme, rPower, kappa, splineOrder, aDim, bDim, cDim, maxKA, maxKB, maxKC, scaleFactor, nThreads,
-        )
+    @ccall libhelpme.helpme_setup_compressedD(
+        pme::Ptr{PMEInstance},
+        rPower::Cshort,
+        kappa::Cdouble,
+        splineOrder::Cint,
+        aDim::Cint,
+        bDim::Cint,
+        cDim::Cint,
+        maxKA::Cint,
+        maxKB::Cint,
+        maxKC::Cint,
+        scaleFactor::Cdouble,
+        nThreads::Cint,
+    )::Cvoid
 end
 
 function helpme_set_lattice_vectorsD(pme::Ptr{PMEInstance}, A, B, C, alpha, beta, gamma, LatticeType)
-    ccall(
-        (:helpme_set_lattice_vectorsD, libhelpme), Cvoid,
-        (Ptr{PMEInstance}, Cdouble, Cdouble, Cdouble, Cdouble, Cdouble, Cdouble, Cint),
-        pme, A, B, C, alpha, beta, gamma, LatticeType,
-        )
+    @ccall libhelpme.helpme_set_lattice_vectorsD(
+        pme::Ptr{PMEInstance},
+        A::Cdouble,
+        B::Cdouble,
+        C::Cdouble,
+        alpha::Cdouble,
+        beta::Cdouble,
+        gamma::Cdouble,
+        LatticeType::Cint,
+    )::Cvoid
 end
 
 function helpme_compute_E_recD(pme::Ptr{PMEInstance}, nAtoms, parameterAngMom, parameters, coordinates)
-    ccall(
-        (:helpme_compute_E_recD, libhelpme), Cdouble,
-        (Ptr{PMEInstance}, Csize_t, Cint, Ref{Cdouble}, Ref{Cdouble}),
-        pme, nAtoms, parameterAngMom, parameters, coordinates,
-        )
+    @ccall libhelpme.helpme_compute_E_recD(
+        pme::Ptr{PMEInstance},
+        nAtoms::Csize_t,
+        parameterAngMom::Cint,
+        parameters::Ref{Cdouble},
+        coordinates::Ref{Cdouble},
+    )::Cdouble
 end
 
 function helpme_compute_EF_recD(pme::Ptr{PMEInstance}, nAtoms, parameterAngMom, parameters, coordinates, forces)
-    ccall(
-        (:helpme_compute_EF_recD, libhelpme), Cdouble,
-        (Ptr{PMEInstance}, Csize_t, Cint, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}),
-        pme, nAtoms, parameterAngMom, parameters, coordinates, forces,
-        )
+    @ccall libhelpme.helpme_compute_EF_recD(
+        pme::Ptr{PMEInstance},
+        nAtoms::Csize_t,
+        parameterAngMom::Cint,
+        parameters::Ref{Cdouble},
+        coordinates::Ref{Cdouble},
+        forces::Ref{Cdouble},
+    )::Cdouble
 end
 
 function helpme_compute_EFV_recD(pme::Ptr{PMEInstance}, nAtoms, parameterAngMom, parameters, coordinates, forces, virial)
-    ccall(
-        (:helpme_compute_EFV_recD, libhelpme), Cdouble,
-        (Ptr{PMEInstance}, Csize_t, Cint, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}),
-        pme, nAtoms, parameterAngMom, parameters, coordinates, forces, virial,
-        )
+    @ccall libhelpme.helpme_compute_EFV_recD(
+        pme::Ptr{PMEInstance},
+        nAtoms::Csize_t,
+        parameterAngMom::Cint,
+        parameters::Ref{Cdouble},
+        coordinates::Ref{Cdouble},
+        forces::Ref{Cdouble},
+        virial::Ref{Cdouble},
+    )::Cdouble
 end
 
 function helpme_compute_P_recD(pme::Ptr{PMEInstance}, nAtoms, parameterAngMom, parameters, coordinates, nGridPoints, gridPoints, derivativeLevel, potential)
-    ccall(
-        (:helpme_compute_P_recD, libhelpme), Cvoid,
-        (Ptr{PMEInstance}, Csize_t, Cint, Ref{Cdouble}, Ref{Cdouble}, Csize_t, Ref{Cdouble}, Cint, Ref{Cdouble}),
-        pme, nAtoms, parameterAngMom, parameters, coordinates, nGridPoints, gridPoints, derivativeLevel, potential,
-        )
+    @ccall libhelpme.helpme_compute_P_recD(
+        pme::Ptr{PMEInstance},
+        nAtoms::Csize_t,
+        parameterAngMom::Cint,
+        parameters::Ref{Cdouble},
+        coordinates::Ref{Cdouble},
+        nGridPoints::Csize_t,
+        gridPoints::Ref{Cdouble},
+        derivativeLevel::Cint,
+        potential::Ref{Cdouble},
+    )::Cvoid
 end
 
 end # module
